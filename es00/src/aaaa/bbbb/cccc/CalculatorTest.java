0424b45570bc32a213d57615a1700fdf49887015
KTE <KTE@KTE-PC>
2020-08-21 10:42:28 +0900

    20200821 exam

---
+++ es00/src/aaaa/bbbb/cccc/CalculatorTest.java
@@ -0,0 +1,18 @@
+package aaaa.bbbb.cccc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void evaluatesExpression() {
+		Calculator calculator = new Calculator();
+		int sum = calculator.evaluate("1+2+3");
+		assertEquals(6,sum);
+
+	}
+
+}
